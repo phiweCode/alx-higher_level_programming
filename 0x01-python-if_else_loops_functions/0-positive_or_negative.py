767240f6a2aa5f7f736249b8a0d252bb74da1cec
phiweCode <thabo.s.mngoma@gmail.com>
2023-03-07 16:59:00 +0200

    This checks whether a number is negative or positive

---
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10, 10)
+
+if number == 0:
+    print("is zero")
+elif number < 0:
+    print("is negative")
+else:
+    number > 0
+    print("is positive")
+
+print("\n")
---
+++ 0x01-python-if_else_loops_functions/README.md
@@ -0,0 +1,1 @@
+This is the if, else, loops and function chapter of the alx program 
