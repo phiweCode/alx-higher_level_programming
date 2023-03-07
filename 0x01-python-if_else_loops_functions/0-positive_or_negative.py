4a981d6299de137e215ba55e49dd363c9d7da4d6
phiweCode <thabo.s.mngoma@gmail.com>
2023-03-07 17:13:46 +0200

    Formatted the output string and then added the inline newline character

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -3,11 +3,11 @@ import random
 number = random.randint(-10, 10)
 
 if number == 0:
-    print("is zero")
+    print("is zero \n")
 elif number < 0:
-    print("is negative")
+    print(f"{number} is negative \n")
 else:
     number > 0
-    print("is positive")
+    print(f"{number} is positive \n")
 
-print("\n")
+
