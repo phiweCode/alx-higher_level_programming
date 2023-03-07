39ec2a1d55c005e8cf7c6523427e5274d58e30d5
phiweCode <thabo.s.mngoma@gmail.com>
2023-03-07 21:12:09 +0200

    adapted the last digit variable to negative numbers

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-lastDigit = number % 10
+
+if number < 0:
+    lastDigit = ((number * -1) % 10) * -1
+else:
+    lastDigit = number % 10
 
 if lastDigit == 0:
     print(f"Last digit of {number} is {lastDigit} and is 0", end="\n")
