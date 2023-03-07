11b37cf681cc71cac2adcee6e9ef175c1f32d4ea
phiweCode <thabo.s.mngoma@gmail.com>
2023-03-07 20:29:22 +0200

    added the newline char at the end keyword

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -3,11 +3,9 @@ import random
 number = random.randint(-10, 10)
 
 if number == 0:
-    print("0 is zero",end="")
+    print("0 is zero", end="\n")
 elif number < 0:
-    print(f"{number} is negative",end="")
+    print(f"{number} is negative", end="\n")
 else:
     number > 0
-    print(f"{number} is positive",end="")
-
-print('\n')
+    print(f"{number} is positive", end="\n")
