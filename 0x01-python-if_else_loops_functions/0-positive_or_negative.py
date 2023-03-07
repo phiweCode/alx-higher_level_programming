e8a1faf5edddc474a7440725118285560257393e
phiweCode <thabo.s.mngoma@gmail.com>
2023-03-07 20:25:33 +0200

    added the end keyword in the print statement

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -3,11 +3,11 @@ import random
 number = random.randint(-10, 10)
 
 if number == 0:
-    print("0 is zero")
+    print("0 is zero", end="")
 elif number < 0:
-    print(f"{number} is negative")
+    print(f"{number} is negative", end="")
 else:
     number > 0
-    print(f"{number} is positive")
+    print(f"{number} is positive", end="")
 
 print('\n')
