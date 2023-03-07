8494f7bc668a52d01712b5333317bebc5e7fdff9
phiweCode <thabo.s.mngoma@gmail.com>
2023-03-07 21:04:30 +0200

    adjusted to the pycodestyle and changed the if condition variable

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,12 +3,13 @@ import random
 number = random.randint(-10000, 10000)
 lastDigit = number % 10
 
-if number == 0:
+if lastDigit == 0:
     print(f"Last digit of {number} is {lastDigit} and is 0", end="\n")
 
-if number > 5:
-    print(f"Last digit of {number} is {lastDigit} greater than 5", end="\n")
+elif lastDigit > 5:
+    print(f"Last digit of {number} is {lastDigit}", end="")
+    print(f" and is greater than 5", end="\n")
 
-if number < 6 and number != 0:
+elif lastDigit < 6 and number != 0:
     print(f"Last digit of {number} is {lastDigit} ", end="")
     print(f"and is less than 6 and not 0", end="\n")
